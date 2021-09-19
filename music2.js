624625cd9d600a2ead610415cbc4c427f6a324aa
Hardcodex1 <iamrf40@gmail.com>
2021-09-19 21:10:58 +0530

    fixed bug

--- music2.js
+++ music2.js
@@ -516,6 +516,10 @@ client.on(`interactionCreate`, async interaction =>
 
 client.on("interactionCreate", async interaction =>
 {
+    if(!interaction.member.voice.channelId) return await interaction.reply({ content: "You are not in a voice channel!", ephemeral: true });
+    if (interaction.guild.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.me.voice.channelId) return await interaction.reply({ content: "You are not in my voice channel!", ephemeral: true });
+
+
     if (interaction.customId == "volume+")
     {
         let queue = player.getQueue(interaction.guild)
